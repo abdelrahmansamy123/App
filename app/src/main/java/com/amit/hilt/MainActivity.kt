672a6f85ace890ffb921207762c928bc7c704079
Abdelrahman Samy <abdelrahmansamy123>
2024-09-27 22:18:56 +0300

    add permission internet

--- app/src/main/java/com/amit/hilt/MainActivity.kt
+++ app/src/main/java/com/amit/hilt/MainActivity.kt
@@ -4,42 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.amit.hilt.ui.screen.MainScreen
 import com.amit.hilt.ui.theme.HiltTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HiltTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Sami", modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "  Hello $name !", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, showSystemUi = true, device = "id:pixel_7_pro")
-@Composable
-fun GreetingPreview() {
-    HiltTheme {
-        Greeting("Sami")
     }
 }
--- app/src/main/java/com/amit/hilt/ui/screen/MainScreen.kt
+++ app/src/main/java/com/amit/hilt/ui/screen/MainScreen.kt
@@ -1,20 +1,32 @@
 package com.amit.hilt.ui.screen
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+
 
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel = hiltViewModel()) {
     val list by viewModel.postsList.collectAsState()
 
     LazyColumn {
-        items(list) {
-            Text(it.title)
+        items(list) { post ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = post.title, modifier = Modifier.padding(16.dp))
+            }
         }
     }
 }
